2ee903ec15b1ef824571ba93e784193949a149fa
hakun0 <brithon.saniko@gmail.com>
2024-01-19 23:07:04 +0100

    algotestsssssssss

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,89 +1,42 @@
 #include "sort.h"
-/**
- * swap_backward -swap two nodes right left position
- * @c: list
- **/
-void swap_backward(listint_t *c)
-{
-	listint_t *tmp, *head;
 
-	while (c->prev != NULL)
-	{
-		if (c->n < c->prev->n)
-		{
-			tmp = c->prev->prev;
-			c->prev->next = c->next;
-			c->next = c->prev;
-			c->prev->prev = c;
-			c->prev = tmp;
-			c->next->next->prev = c->next;
-			if (tmp != NULL)
-				tmp->next = c;
-			head = c;
-			while (head->prev != NULL)
-				head = head->prev;
-			print_list(head);
-		}
-		else
-			c = c->prev;
-	}
-}
 /**
- * swap_forward
+ * insertion_sort_list - function
+ * @list: list to sort
  */
-void swap_forward(listint_t *c)
-{
-	listint_t *tmp, *head;
-
-	tmp = c->prev;
-
-	if (tmp != NULL)
-	{
-		tmp->next = c->next;
-		c->next->prev = tmp;
-	}
-	else
-		c->next->prev = NULL;
-	c->prev = c->next;
-	if (c->next->next != NULL)
-	{
-		c->next = c->next->next;
-		c->prev->next = c;
-		c->next->prev = c;
-	}
-	else
-	{
-		c->next->next = c;
-		c->next = NULL;
-	}
-	head = c;
-	while (head->prev != NULL)
-		head = head->prev;
-	print_list(head);
-	swap_backward(c->prev);
-}
-/**
- * insertion_sort_list -sort a doubly linked list with insert algorithm
- * @list: list
- *
- **/
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *c;
+	listint_t	*head;
+	listint_t	*tmp;
 
-	if ((list == NULL) || (*list == NULL) || ((*list)->next == NULL))
+	if (!list)
 		return;
-	c = *list;
-
-	while (c->next != NULL)
+	if (!*list)
+		return;
+	head = *list;
+	while (head)
 	{
-		if (c->n > c->next->n)
+		while (head->next && (head->n > head->next->n))
 		{
-			swap_forward(c);
+			tmp = head->next;
+			head->next = tmp->next;
+			tmp->prev = head->prev;
+
+			if (head->prev)
+				head->prev->next = tmp;
+			if (tmp->next)
+				tmp->next->prev = head;
+
+			head->prev = tmp;
+			tmp->next = head;
+
+			if (tmp->prev)
+				head = tmp->prev;
+			else
+				*list = tmp;
+
+			print_list(*list);
 		}
-		else
-			c = c->next;
+		head = head->next;
 	}
-	while ((*list)->prev != NULL)
-		*list = (*list)->prev;
 }
